92fd64a86197b98ab8b4261f2c53d07627d2b523
Cyril Marchive <cyril.marchive@gmail.com>
2019-05-19 19:53:04 -0400

    Ajout de donnees au demarrage de l'application

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-delete from operation;
-delete from operation_permanente;
-delete from utilisateur;
-
-insert into utilisateur(id, nom, prenom, email) values(1, 'Marchive', 'Cyril', 'cyril.marchive@gmail.com');
-insert into utilisateur(id, nom, prenom, email) values(2, 'Boussat', 'Melanie', 'melanie.boussat@gmail.com');
