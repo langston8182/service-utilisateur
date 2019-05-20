5f6cf431fd9571731c6fc7b7c01b7c4d66c3f7f9
Cyril Marchive <cyril.marchive@gmail.com>
2019-05-20 13:00:31 -0400

    Ajout de l'id okta dans les donnees sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,5 @@ delete from operation;
 delete from operation_permanente;
 delete from utilisateur;
 
-insert into utilisateur(id, nom, prenom, email) values(1, 'Marchive', 'Cyril', 'cyril.marchive@gmail.com');
-insert into utilisateur(id, nom, prenom, email) values(2, 'Boussat', 'Melanie', 'melanie.boussat@gmail.com');
+insert into utilisateur(id, id_okta, nom, prenom, email) values(1, '00ukz1wxxtbR97LJA356', 'Marchive', 'Cyril', 'cyril.marchive@gmail.com');
+insert into utilisateur(id, id_okta, nom, prenom, email) values(2, '', 'Boussat', 'Melanie', 'melanie.boussat@gmail.com');
