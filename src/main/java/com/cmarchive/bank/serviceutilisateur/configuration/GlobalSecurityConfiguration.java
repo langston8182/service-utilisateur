e3e5a4d9120b0ed21d7a4d1cdb2025fe8d7b4b59
Cyril Marchive <cyril.marchive@gmail.com>
2019-05-20 11:27:15 -0400

    Prise en compte des scopes pour la securite

---
+++ src/main/java/com/cmarchive/bank/serviceutilisateur/configuration/GlobalSecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.cmarchive.bank.serviceutilisateur.configuration;
+
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+
+    @Override
+    protected MethodSecurityExpressionHandler createExpressionHandler() {
+        return new OAuth2MethodSecurityExpressionHandler();
+    }
+}
