d0e6bef1df26471fb370c724cc74801efa249cf1
Cyril Marchive <cyril.marchive@gmail.com>
2019-05-21 18:46:54 -0400

    Mise en place swagger

---
+++ src/main/java/com/cmarchive/bank/serviceutilisateur/configuration/ServeurSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.cmarchive.bank.serviceutilisateur.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ServeurSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }
+}
