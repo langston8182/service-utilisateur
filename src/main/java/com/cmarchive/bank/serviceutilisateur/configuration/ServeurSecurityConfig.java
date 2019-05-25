17f30db9d4bb78f5475ff24dd5c636c935ee639e
Cyril Marchive <cyril.marchive@gmail.com>
2019-05-24 21:23:28 -0400

    Serveur Stateless

--- src/main/java/com/cmarchive/bank/serviceutilisateur/configuration/ServeurSecurityConfig.java
+++ src/main/java/com/cmarchive/bank/serviceutilisateur/configuration/ServeurSecurityConfig.java
@@ -1,11 +1,19 @@
 package com.cmarchive.bank.serviceutilisateur.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class ServeurSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
 
     @Override
     public void configure(WebSecurity web) {
