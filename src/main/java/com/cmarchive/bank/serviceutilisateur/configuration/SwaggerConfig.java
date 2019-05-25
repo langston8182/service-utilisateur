b03c1f012afbe8813fca5a1e5f6d79ae91e50337
Cyril Marchive <cyril.marchive@gmail.com>
2019-05-24 22:28:14 -0400

    Modification du docket swagger

--- src/main/java/com/cmarchive/bank/serviceutilisateur/configuration/SwaggerConfig.java
+++ src/main/java/com/cmarchive/bank/serviceutilisateur/configuration/SwaggerConfig.java
@@ -34,7 +34,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.cmarchive.bank.ressource.api"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
                 .securitySchemes(asList(securityScheme()))
